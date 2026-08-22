f3914932448b4b94d737be1545cb976c3e92cd25
Merisorj <74406461+Merisorj@users.noreply.github.com>
2026-08-22 22:44:41 +0300

    Update blackjack game logic
    Added hit/stand handling, bust detection, blackjack detection and dealer drawing logic.

--- MiniBlackJack.cs
+++ MiniBlackJack.cs
@@ -2,7 +2,7 @@ Random rand = new();
 
 // 1. Tragi prima carte si a doua carte pentru jucator
 int card1 = rand.Next(1, 12);
-int card2 = rand.Next(1,12);
+int card2 = rand.Next(1, 12);
 int dealerCard1 = rand.Next(1,12);
 int dealerCard2 = rand.Next(1,12);
 
@@ -18,44 +18,54 @@ Console.WriteLine($"Dealer pulled {dealerCard1}");
 Console.WriteLine($"Dealer score: {dealerScore}\n\n");
 
 string? playerChoice = "";
-
-do
+if(playerScore == 21)
 {
-    Console.WriteLine("Hit or stand? (Hit/H or Stand/S)");
-    playerChoice = Console.ReadLine()?.ToLower();
-    
-    if(playerChoice =="hit" || playerChoice =="h")
+    Console.WriteLine("Congratulations! You've just hit a BlackJack!");
+}
+
+else
+{
+    do
     {
-        int pulledCard = rand.Next(1,12);
-        playerScore += pulledCard;
-        Console.WriteLine($"\nYou pulled {pulledCard}. Current Score: {playerScore}\n");
-        if (playerScore > 21)
+        Console.WriteLine("Hit or stand? (Hit/H or Stand/S)");
+        playerChoice = Console.ReadLine()?.ToLower();
+        
+        if(playerChoice =="hit" || playerChoice =="h" && playerScore < 21)
         {
-            Console.WriteLine($"That's a bust! You lose with {playerScore}");
-            break;
-        }
-    }
-} while(playerChoice == "hit" || playerChoice == "h" && playerScore <=21);
+            int pulledCard = rand.Next(1,12);
+            playerScore += pulledCard;
+            Console.WriteLine($"\nYou pulled {pulledCard}. Current Score: {playerScore}\n");
+            if (playerScore > 21)
+            {
+                Console.WriteLine($"That's a bust! You lose with {playerScore}");
+                break;
+            }
 
-if ((playerChoice == "stand") || (playerChoice =="s"))
+        }
+    } while((playerChoice == "hit" || playerChoice == "h") && playerScore <21);
+}
+
+
+if (playerChoice == "stand" || playerChoice =="s" || playerScore == 21)
 {
+    
         Console.WriteLine($"Current Player Score: {playerScore}");
             while(dealerScore < 17)
             {
                 int dealerCard = rand.Next(1,12);
                 dealerScore +=dealerCard;
-
                 Console.WriteLine($"Dealer pulls a card({dealerCard}). Dealer Score: {dealerScore}.");
-                if(dealerScore > 21 || playerScore > dealerScore)
-                {
-                    Console.WriteLine($"You WIN! Player: {playerScore} vs Dealer: {dealerScore}");
-                }
-                else if(dealerScore == playerScore)
-                {
-                    Console.WriteLine($"It's a DRAW at {playerScore} points!");
-                }
-                
+            }    
+            if(dealerScore > 21 || playerScore > dealerScore)
+            {
+                Console.WriteLine($"You WIN! Player: {playerScore} vs Dealer: {dealerScore}");
             }
+            else if(dealerScore == playerScore)
+            {
+                Console.WriteLine($"It's a DRAW at {playerScore} points!");
+            }
+                
+            
             if((dealerScore > playerScore) && (dealerScore ==21 || dealerScore <=21))
                 {
                     Console.WriteLine($"Dealer Wins with Score {dealerScore}.");
