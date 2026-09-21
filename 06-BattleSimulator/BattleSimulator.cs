029cd27fc8ac29133122ba805d70206a08aaea38
Merisorj <andrei_justinian3@yahoo.com>
2026-09-21 11:19:03 +0300

    Learned methods and completed BattleSimulator mini-project

--- 06-BattleSimulator/BattleSimulator.cs
+++ 06-BattleSimulator/BattleSimulator.cs
@@ -2,18 +2,51 @@ void AfiseazaTitlu()
 {
     Console.WriteLine("=== BATTLE SIMULATOR ===");
 }
+void AfiseazaStatus(int playerHealth, int enemyHealth)
+{
+
+    Console.WriteLine($"Player Health: {playerHealth}");
+
+    Console.WriteLine($"Enemy Health: {enemyHealth}");
+}
 int playerHealth = 100;
 int enemyHealth = 100;
+
 Random damage = new();
+
+int GenereazaDamage()
+{
+    int generalDamage = damage.Next(10, 26);
+    return generalDamage;
+}
+void AfiseazaRezultat(int playerHealth, int enemyHealth)
+{
+    if (playerHealth <= 0 && enemyHealth <= 0)
+    {
+        Console.WriteLine("Draw!");
+    }
+
+    else if (playerHealth <= 0)
+    {
+        Console.WriteLine("Enemy Wins!");
+
+    }
+    else if (enemyHealth <= 0)
+    {
+        Console.WriteLine("Player Wins!");
+
+    }
+}
 
 AfiseazaTitlu();
 
-while((playerHealth > 0) && (enemyHealth > 0))
+while ((playerHealth > 0) && (enemyHealth > 0))
 {
-    int playerDamage = damage.Next(10,26);
-    int enemyDamage = damage.Next(10,26);
-    playerHealth -=enemyDamage;
-    Console.WriteLine($"Player Health: {playerHealth}");
-    enemyHealth -=playerDamage;
-    Console.WriteLine($"Enemy Health: {enemyHealth}");
+    int playerDamage = GenereazaDamage();
+    int enemyDamage = GenereazaDamage();
+    playerHealth -= enemyDamage;
+    enemyHealth -= playerDamage;
+    AfiseazaStatus(playerHealth, enemyHealth);
+
 }
+AfiseazaRezultat(playerHealth, enemyHealth);
